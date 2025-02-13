30cc1a5301a6c32949814bf735ba0bb4793c49f1
AhmedAbdallah69 <aljwkrl@gmail.com>
2025-02-13 04:12:03 +0200

    done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""Export data from an API to CSV format.
+"""
 import csv
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+if __name__ == '__main__':
+    # Checks if the argument can be converted to a number
+    try:
+        emp_id = int(argv[1])
+    except ValueError:
+        exit()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    # Main formatted names to API uris and filenames
+    api_url = 'https://jsonplaceholder.typicode.com'
+    user_uri = '{api}/users/{id}'.format(api=api_url, id=emp_id)
+    todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
+    filename = '{emp_id}.csv'.format(emp_id=emp_id)
+
+    # User Response
+    res = requests.get(user_uri).json()
+
+    # Username of the employee
+    username = res.get('username')
+
+    # User TODO Response
+    res = requests.get(todo_uri).json()
+
+    # Create the new file for the user to save the information
+    # Filename example: `{user_id}.csv`
+    with open(filename, 'w', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+
+        for elem in res:
+            # Completed or non-completed task
+            status = elem.get('completed')
+
+            # The task name
+            title = elem.get('title')
+
+            # Writing each result of API response in a row of a CSV file
+            writer.writerow([emp_id, username, status, title])
